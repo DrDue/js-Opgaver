7154fef48b4fd3a8ae721e438bcb118d5b4098c6
DrDue <dr.due98@gmail.com>
2020-09-08 16:56:00 +0200

    update

--- js/execution.js
+++ js/execution.js
@@ -1,22 +1,30 @@
 'use strict';
-document.write( `<h1>Assignments</h1>` );
-
-document.write( `<h2>JS.Arrays.1</h2>` );
-
-document.write( `<p>Flip coin: ${flipCoin()}</p>` );
-document.write( `<p>Roll die: ${rollDice()}</p>` );
-
-
-document.write( `<hr/>` );
-
-document.write( `<h2>JS.Arrays.2</h2>` );
-
-let s = prompt( 'tilføj det ord du vil finde ud af om er et palindrom' );
-document.write( `<p>Is ${s} a palindrome: ${isPalindrome(s)}</p>` );
-
-document.write( `<hr/>` );
+// document.write( `<h1>Assignments</h1>` );
+//
+// document.write( `<h2>JS.Arrays.1</h2>` );
+//
+// document.write( `<p>Flip coin: ${flipCoin()}</p>` );
+// document.write( `<p>Roll die: ${rollDice()}</p>` );
+//
+//
+// document.write( `<hr/>` );
+//
+// document.write( `<h2>JS.Arrays.2</h2>` );
+//
+// let s = prompt( 'tilføj det ord du vil finde ud af om er et palindrom' );
+// document.write( `<p>Is ${s} a palindrome: ${isPalindrome(s)}</p>` );
+//
+// document.write( `<hr/>` );
 
 document.write( `<h2>JS.Arrays.3</h2>` );
+
+let x = Number( window.prompt( `How many time do you wanna roll the dice?` ) );
+let arr = play( x );
+if ( x in arr ) {
+  document.write( `alle terninger har ramt den samme :o` );
+} else {
+  document.write( `Alle terninger har ikke slået det samme :(` );
+}
 
 // let c = Number( window.prompt( 'Enter temp in C' ) );
 // document.write( `<p>${c} &deg;C = ${C2F(c)}&deg;F</p>` );
--- js/jsArrays3.js
+++ js/jsArrays3.js
@@ -1,1 +1,34 @@
 'use strict'
+
+
+
+function play( j ) {
+  let dice = [ 0, 0, 0, 0, 0, 0, 0 ]
+
+  for ( let i = 0; i < j; i++ ) {
+    let x = Math.random();
+    x = Math.floor( x * 6 + 1 );
+    dice[ x ]++;
+    console.log( dice );
+  }
+  return dice;
+}
+
+//roll <= i
+
+//let rolls = Number( window.prompt( `How many time do you wanna roll the dice?` ) );
+// const funk123 = function ( rolls ) {
+//   var dice1 = ;
+//   for ( var i = 0; i < rolls; i++ ) {
+//     var dieValue = [ 1, 2, 3, 4, 5, 6 ];
+//     var randomRoll = Math.ceil( Math.random() * dieValue.length );
+//
+//     if ( randomRoll in dice1 ) {
+//       dice1[ randomRoll ]++;
+//     } else {
+//       dice1[ randomRoll ] = 1;
+//     }
+//   }
+//
+//   console.log( dice1 );
+// }
