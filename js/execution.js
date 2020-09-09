3a936fdea809dd3bc3e8153ecfe25e98ddc3febb
DrDue <70370132+DrDue@users.noreply.github.com>
2020-09-09 13:26:53 +0200

    Update execution.js

--- js/execution.js
+++ js/execution.js
@@ -1,30 +1,33 @@
 'use strict';
-// document.write( `<h1>Assignments</h1>` );
-//
-// document.write( `<h2>JS.Arrays.1</h2>` );
-//
-// document.write( `<p>Flip coin: ${flipCoin()}</p>` );
-// document.write( `<p>Roll die: ${rollDice()}</p>` );
-//
-//
-// document.write( `<hr/>` );
-//
-// document.write( `<h2>JS.Arrays.2</h2>` );
-//
-// let s = prompt( 'tilføj det ord du vil finde ud af om er et palindrom' );
-// document.write( `<p>Is ${s} a palindrome: ${isPalindrome(s)}</p>` );
-//
-// document.write( `<hr/>` );
+document.write( `<h1>Assignments</h1>` );
+
+document.write( `<h2>JS.Arrays.1</h2>` );
+
+document.write( `<p>Flip coin: ${flipCoin()}</p>` );
+document.write( `<p>Roll die: ${rollDice()}</p>` );
+
+
+document.write( `<hr/>` );
+
+document.write( `<h2>JS.Arrays.2</h2>` );
+
+let s = prompt( 'tilføj det ord du vil finde ud af om er et palindrom' );
+document.write( `<p>Is ${s} a palindrome: ${isPalindrome(s)}</p>` );
+
+document.write( `<hr/>` );
 
 document.write( `<h2>JS.Arrays.3</h2>` );
 
 let x = Number( window.prompt( `How many time do you wanna roll the dice?` ) );
 let arr = play( x );
-if ( x in arr ) {
-  document.write( `alle terninger har ramt den samme :o` );
+if ( arr.includes(x) ) {
+  document.write( `<p>alle terninger har ramt den samme :o</p>` );
 } else {
-  document.write( `Alle terninger har ikke slået det samme :(` );
+  document.write( `<p>Alle terninger har ikke slået det samme :(</p>` );
 }
+
+let y = Number( window.prompt(`What dice number (1-6) will you see the amount it's been rolled?`));
+document.write(`<p>the number ${y} has been rolled ${arr[y]} times</p>`);
 
 // let c = Number( window.prompt( 'Enter temp in C' ) );
 // document.write( `<p>${c} &deg;C = ${C2F(c)}&deg;F</p>` );
